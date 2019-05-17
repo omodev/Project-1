ed01d5bb305527732c127289d52b9713d686edff
Omar Hassane <hassaneomar95@gmail.com>
2019-05-16 23:20:28 -0400

    server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+
+/**
+* @file Root server file.
+* @version 1.0.0
+* @author Omar
+ */
+
 'use strict'    // Strict Mode
+
+
+/**
+ * Module dependencies.
+ */
+
+const http = require('http');
+const fs = require("fs") ;
+
+/**
+* Creating the http server
+*/
+var server = http.createServer((req, res)=> {
+
+    // Create a readStream (source from which we can read)
+     var readStream = fs.createReadStream(__dirname+"/data.txt");
+
+     // Pipe whatever is being read from the source to the res
+      readStream.pipe(res) ;
+
+}); 
+
+server.listen(3000) ;
